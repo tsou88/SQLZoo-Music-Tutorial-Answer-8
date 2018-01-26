63ecb3de77b0b70aa4f1cecfb4c296ced9f574ad
tsou88 <35821929+tsou88@users.noreply.github.com>
2018-01-26 14:31:29 +0800

    Update Music_Tutorial_9.sql

--- Music_Tutorial_9.sql
+++ Music_Tutorial_9.sql
@@ -1,3 +1,5 @@
+/*#9 A "good value" album is one where the price per track is less than 50 pence. Find the good value album - show the title, the price and the number of tracks.*/
+
 SELECT title, price, COUNT(song)
 FROM album JOIN track ON (album.asin=track.album)
 GROUP BY title, price
