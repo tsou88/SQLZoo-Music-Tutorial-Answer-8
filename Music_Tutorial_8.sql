38c01355d46a2931190001b6212988b77f015b51
tsou88 <35821929+tsou88@users.noreply.github.com>
2018-01-26 14:29:46 +0800

    Update Music_Tutorial_8.sql

--- Music_Tutorial_8.sql
+++ Music_Tutorial_8.sql
@@ -1,3 +1,5 @@
+/*#8 Find the songs that appear on more than 2 albums. Include a count of the number of times each shows up.*/
+
 SELECT song, COUNT(DISTINCT album)
 FROM album JOIN track ON (album.asin = track.album)
 GROUP BY song
