9359c1eaf9e2972feef6ff841a505160480a38b3
tsou88 <35821929+tsou88@users.noreply.github.com>
2018-01-26 08:23:40 +0800

    Update Music_8.sql

--- Music_8.sql
+++ Music_8.sql
@@ -1,4 +1,4 @@
 SELECT song, COUNT(DISTINCT album)
-FROM album JOIN track ON (album.asin=track.album)
+FROM album JOIN track ON (album.asin = track.album)
 GROUP BY song
 HAVING COUNT(DISTINCT album) > 2
